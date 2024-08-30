tq-systems/tq-imx-sm

commit d2e1f9621f756e7ef1d2e86492ed8b31148dd12f

sm: boot: add some informal output
Helps to understand config errors and the like.

Signed-off-by: Markus Niebel <Markus.Niebel@ew.tq-group.com>

sm/boot/sm.c
@@ -79,6 +79,7 @@ int main(int argc, const char * const argv[])
 
     /* Init the system hardware */
     status = BRD_SM_Init(argc, argv, &mSel);
+    printf("BRD_SM_Init: %d)\n", status);
 
     /* Save start banner time */
     delta = DEV_SM_Usec64Get();
@@ -94,6 +95,7 @@ int main(int argc, const char * const argv[])
     if (status == SM_ERR_SUCCESS)
     {
         status = LMM_Init();
+        printf("LMM_Init: %d)\n", status);
     }
 
     /* Boot LMs */
@@ -101,6 +103,7 @@ int main(int argc, const char * const argv[])
     {
         /* mSel from BRD_SM_Init(), LMM_INIT_FLAGS from Makefile */
         status = LMM_Boot(mSel, LMM_INIT_FLAGS);
+        printf("LMM_Boot: %d)\n", status);
     }
 
 #ifdef RUN_TEST
@@ -108,6 +111,7 @@ int main(int argc, const char * const argv[])
     if (status == SM_ERR_SUCCESS)
     {
         status = TEST_Config();
+        printf("TEST_Config: %d)\n", status);
     }
 #endif
 
@@ -121,16 +125,18 @@ int main(int argc, const char * const argv[])
     /* Run test */
     if (status == SM_ERR_SUCCESS)
     {
+        printf("\n*** SM Test Mode - no further booting ***\n");
         TEST();
     }
 #endif
 
 #ifdef MONITOR
     /* Call monitor */
-    MONITOR_Cmd("\n*** SM Debug Monitor ***\n");
+    MONITOR_Cmd("\n*** SM Debug Monitor - no further booting ***\n");
 #endif
 
 #if !defined(RUN_TEST) && !defined(MONITOR)
+    printf("\n*** SM Main Loop ***\n");
     /* Loop - services handled via interrupts */
     while(true)
     {

commit f6cb7ed4774779c3b63f1f5b177a8c0c9e31ce3a

tqma95xxsa: remove superfluous character in tracing output
In some trace output was a ')' sign after the printed return code.
Remove this.

Fixes: d2e1f9621f75 ("sm: boot: add some informal output")

Signed-off-by: Markus Niebel <Markus.Niebel@ew.tq-group.com>

boards/tqma95xxsa/sm/brd_sm.c
@@ -141,7 +141,7 @@ int32_t BRD_SM_Init(int argc, const char * const argv[], uint32_t *mSel)
 
     /* Initialize devices connected to serial buses (PMIC, IOExp, etc) */
     status = BRD_SM_SerialDevicesInit();
-    printf("BRD_SM_SerialDevicesInit: %d)\n", status);
+    printf("BRD_SM_SerialDevicesInit: %d\n", status);
 
     if (status == SM_ERR_SUCCESS)
     {

sm/boot/sm.c
@@ -84,7 +84,7 @@ int main(int argc, const char * const argv[])
 
     /* Init the system hardware */
     status = BRD_SM_Init(argc, argv, &mSel);
-    printf("BRD_SM_Init: %d)\n", status);
+    printf("BRD_SM_Init: %d\n", status);
 
     /* Save start banner time */
     delta = DEV_SM_Usec64Get();
@@ -102,14 +102,14 @@ int main(int argc, const char * const argv[])
     {
         /* mSel from BRD_SM_Init(), LMM_INIT_FLAGS from Makefile */
         status = LMM_Init(&mSel, LMM_INIT_FLAGS);
-        printf("LMM_Init: %d)\n", status);
+        printf("LMM_Init: %d\n", status);
     }
 
     /* Boot LMs */
     if (status == SM_ERR_SUCCESS)
     {
         status = LMM_Boot();
-        printf("LMM_Boot: %d)\n", status);
+        printf("LMM_Boot: %d\n", status);
     }
 
 #ifdef RUN_TEST
@@ -117,7 +117,7 @@ int main(int argc, const char * const argv[])
     if (status == SM_ERR_SUCCESS)
     {
         status = TEST_Config();
-        printf("TEST_Config: %d)\n", status);
+        printf("TEST_Config: %d\n", status);
     }
 #endif
 