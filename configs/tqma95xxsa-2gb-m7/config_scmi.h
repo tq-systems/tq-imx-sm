f637ef5733eec34b28d673a48619731660414a06
Alexander Stein <alexander.stein@ew.tq-group.com>
2025-08-04 16:46:57 +0200

    configs/tqma95xxsa-2gb-m7: Remove PERLPI not required
    LPUART3 is unused
    
    Signed-off-by: Alexander Stein <alexander.stein@ew.tq-group.com>

--- configs/tqma95xxsa-2gb-m7/config_scmi.h
+++ configs/tqma95xxsa-2gb-m7/config_scmi.h
@@ -88,7 +88,6 @@
         .lmmPerms[2] = SM_SCMI_PERM_ALL, \
         .pdPerms[DEV_SM_PD_M7] = SM_SCMI_PERM_ALL, \
         .perfPerms[DEV_SM_PERF_M7] = SM_SCMI_PERM_ALL, \
-        .perlpiPerms[DEV_SM_PERLPI_LPUART3] = SM_SCMI_PERM_ALL, \
         .perlpiPerms[DEV_SM_PERLPI_LPUART5] = SM_SCMI_PERM_ALL, \
         .perlpiPerms[DEV_SM_PERLPI_WDOG5] = SM_SCMI_PERM_ALL, \
         .pinPerms[DEV_SM_PIN_GPIO_IO00] = SM_SCMI_PERM_ALL, \
