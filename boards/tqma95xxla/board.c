6a0d9f971b71be9b4154b9efe2239d63717ab811
Markus Niebel <Markus.Niebel@ew.tq-group.com>
2026-02-18 13:20:13 +0100

    tqma95xxsa/tqma95xxla: Merge common SDK changes
    Changes according to commit 774d95d3b4d7 ("SM-277: Merge common SDK changes.")
    
    Signed-off-by: Markus Niebel <Markus.Niebel@ew.tq-group.com>

--- boards/tqma95xxla/board.c
+++ boards/tqma95xxla/board.c
@@ -113,8 +113,8 @@ void BOARD_ConfigMPU(void)
     uint8_t attr;
 
     /* Disable code cache(ICache) and system cache(DCache) */
-    XCACHE_DisableCache(LPCAC_PC);
-    XCACHE_DisableCache(LPCAC_PS);
+    XCACHE_DisableCache(M33_CACHE_CTRLPC);
+    XCACHE_DisableCache(M33_CACHE_CTRLPS);
 
     /* NOTE: All TCRAM is non-cacheable regardless of MPU setting. */
 
@@ -237,8 +237,8 @@ void BOARD_ConfigMPU(void)
     ARM_MPU_Enable(MPU_CTRL_PRIVDEFENA_Msk);
 
     /* Enable ICache and DCache */
-    XCACHE_EnableCache(LPCAC_PC);
-    XCACHE_EnableCache(LPCAC_PS);
+    XCACHE_EnableCache(M33_CACHE_CTRLPC);
+    XCACHE_EnableCache(M33_CACHE_CTRLPS);
 }
 
 /*--------------------------------------------------------------------------*/
