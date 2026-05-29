tq-systems/tq-imx-sm

commit 07c1bf78ec5e28edcf560042295bca11e8624267

tqma94xxla/tqma95xxla/tqma95xxsa: SM-339: Allow V2X fast hash after ROM phase
Changes according to commit 5c1ed13e8b3d ("SM-339: Allow V2X fast hash after ROM phase")

Signed-off-by: Markus Niebel <Markus.Niebel@ew.tq-group.com>

configs/tqma95xxla-4gb.cfg
@@ -72,6 +72,9 @@ DATA:               perm=rw
 
 # Resources
 
+# Sharing MP access may not be safe if FuSa SW using EDMA2
+EDMA2_MP            ACCESS
+EDMA2_CH0_1         OWNER
 V2X_ACC             OWNER
 
 # Memory
@@ -103,7 +106,9 @@ V2X_FH              OWNER
 
 # Memory
 
+DDR                 READONLY, begin=0x080000000, end=0x08AFFFFFF, nodbg
 DDR                 DATA, begin=0x08B000000, end=0x08BFFFFFF, nodbg
+DDR                 READONLY, begin=0x08C000000, end=0x17FFFFFFF, nodbg
 
 #==========================================================================#
 # SM M33 EENV                                                              #

configs/tqma95xxla-4gb/config_trdc.h
@@ -689,6 +689,7 @@
         SM_CFG_W1(0x000008e0U), 0xB00000ACU, \
         SM_CFG_W1(0x00000900U), 0x90000004U, \
         SM_CFG_W1(0x00000920U), 0xB0000053U, \
+        SM_CFG_W1(0x00000920U), 0xB00000A0U, \
         SM_CFG_W1(0x00000940U), 0xB0000053U, \
         SM_CFG_W1(0x00000960U), 0xB0000053U, \
         SM_CFG_W1(0x00000980U), 0xB0000053U, \
@@ -952,8 +953,9 @@
         SM_CFG_W1(0x00018580U), 0x33333333U, \
         SM_CFG_W1(0x00019240U), 0x99999999U, \
         SM_CFG_W1(0x00019380U), 0x99999999U, \
-        SM_CFG_W1(0x0001a024U), 0x7777U, \
-        SM_CFG_W1(0x0001a028U), 0x7700U, \
+        SM_CFG_W1(0x0001a024U), 0x4444U, \
+        SM_CFG_W1(0x0001a028U), 0x7777U, \
+        SM_CFG_W1(0x0001a02cU), 0x7700U, \
         SM_CFG_W1(0x0001a040U), 0x08000000U, \
         SM_CFG_W1(0x0001a044U), 0x17FFFC11U, \
         SM_CFG_Z1(0x0001a04cU), \
@@ -963,18 +965,18 @@
         SM_CFG_Z1(0x0001a24cU), \
         SM_CFG_Z1(0x0001a254U), \
         SM_CFG_Z1(0x0001a25cU), \
-        SM_CFG_W1(0x0001a340U), 0x08800001U, \
+        SM_CFG_W1(0x0001a340U), 0x08800002U, \
         SM_CFG_W1(0x0001a344U), 0x089FFC11U, \
-        SM_CFG_W1(0x0001a348U), 0x08A00002U, \
+        SM_CFG_W1(0x0001a348U), 0x08A00003U, \
         SM_CFG_W1(0x0001a34cU), 0x08DFFC01U, \
-        SM_CFG_W1(0x0001a350U), 0x08E00001U, \
+        SM_CFG_W1(0x0001a350U), 0x08E00002U, \
         SM_CFG_W1(0x0001a354U), 0x17FFFC11U, \
         SM_CFG_Z1(0x0001a35cU), \
         SM_CFG_Z1(0x0001a364U), \
         SM_CFG_Z1(0x0001a36cU), \
         SM_CFG_Z1(0x0001a374U), \
         SM_CFG_Z1(0x0001a37cU), \
-        SM_CFG_W1(0x0001a440U), 0x08000001U, \
+        SM_CFG_W1(0x0001a440U), 0x08000002U, \
         SM_CFG_W1(0x0001a444U), 0x089FFC11U, \
         SM_CFG_Z1(0x0001a44cU), \
         SM_CFG_Z1(0x0001a454U), \
@@ -1015,10 +1017,12 @@
         SM_CFG_Z1(0x0001ab4cU), \
         SM_CFG_Z1(0x0001ab54U), \
         SM_CFG_Z1(0x0001ab5cU), \
-        SM_CFG_W1(0x0001ac40U), 0x08B00000U, \
-        SM_CFG_W1(0x0001ac44U), 0x08BFFC11U, \
-        SM_CFG_Z1(0x0001ac4cU), \
-        SM_CFG_Z1(0x0001ac54U), \
+        SM_CFG_W1(0x0001ac40U), 0x08000001U, \
+        SM_CFG_W1(0x0001ac44U), 0x08AFFC11U, \
+        SM_CFG_W1(0x0001ac48U), 0x08B00000U, \
+        SM_CFG_W1(0x0001ac4cU), 0x08BFFC11U, \
+        SM_CFG_W1(0x0001ac50U), 0x08C00001U, \
+        SM_CFG_W1(0x0001ac54U), 0x17FFFC11U, \
         SM_CFG_Z1(0x0001ac5cU), \
         SM_CFG_Z1(0x00010020U), \
         SM_CFG_Z1(0x00012020U), \
@@ -1154,6 +1158,7 @@
         SM_CFG_W1(0x00010024U), 0x6666U, \
         SM_CFG_W1(0x00010028U), 0x7777U, \
         SM_CFG_W1(0x0001002cU), 0x6600U, \
+        SM_CFG_W1(0x00010040U), 0x00000033U, \
         SM_CFG_Z1(0x00010060U), \
         SM_CFG_W1(0x00010070U), 0x00003000U, \
         SM_CFG_Z1(0x00010260U), \

configs/tqma95xxsa-2gb-m7.cfg
@@ -72,6 +72,9 @@ DATA:               perm=rw
 
 # Resources
 
+# Sharing MP access may not be safe if FuSa SW using EDMA2
+EDMA2_MP            ACCESS
+EDMA2_CH0_1         OWNER
 V2X_ACC             OWNER
 
 # Memory
@@ -103,7 +106,9 @@ V2X_FH              OWNER
 
 # Memory
 
+DDR                 READONLY, begin=0x080000000, end=0x08AFFFFFF, nodbg
 DDR                 DATA, begin=0x08B000000, end=0x08BFFFFFF, nodbg
+DDR                 READONLY, begin=0x08C000000, end=0x0FFFFFFFF, nodbg
 
 #==========================================================================#
 # SM M33 EENV                                                              #

configs/tqma95xxsa-2gb-m7/config_trdc.h
@@ -689,6 +689,7 @@
         SM_CFG_W1(0x000008e0U), 0xB00000ACU, \
         SM_CFG_W1(0x00000900U), 0x90000004U, \
         SM_CFG_W1(0x00000920U), 0xB0000053U, \
+        SM_CFG_W1(0x00000920U), 0xB00000A0U, \
         SM_CFG_W1(0x00000940U), 0xB0000053U, \
         SM_CFG_W1(0x00000960U), 0xB0000053U, \
         SM_CFG_W1(0x00000980U), 0xB0000053U, \
@@ -952,8 +953,9 @@
         SM_CFG_W1(0x00018580U), 0x33333333U, \
         SM_CFG_W1(0x00019240U), 0x99999999U, \
         SM_CFG_W1(0x00019380U), 0x99999999U, \
-        SM_CFG_W1(0x0001a024U), 0x7777U, \
-        SM_CFG_W1(0x0001a028U), 0x7700U, \
+        SM_CFG_W1(0x0001a024U), 0x4444U, \
+        SM_CFG_W1(0x0001a028U), 0x7777U, \
+        SM_CFG_W1(0x0001a02cU), 0x7700U, \
         SM_CFG_W1(0x0001a040U), 0x08000000U, \
         SM_CFG_W1(0x0001a044U), 0x0FFFFC11U, \
         SM_CFG_Z1(0x0001a04cU), \
@@ -963,18 +965,18 @@
         SM_CFG_Z1(0x0001a24cU), \
         SM_CFG_Z1(0x0001a254U), \
         SM_CFG_Z1(0x0001a25cU), \
-        SM_CFG_W1(0x0001a340U), 0x08800001U, \
+        SM_CFG_W1(0x0001a340U), 0x08800002U, \
         SM_CFG_W1(0x0001a344U), 0x089FFC11U, \
-        SM_CFG_W1(0x0001a348U), 0x08A00002U, \
+        SM_CFG_W1(0x0001a348U), 0x08A00003U, \
         SM_CFG_W1(0x0001a34cU), 0x08DFFC01U, \
-        SM_CFG_W1(0x0001a350U), 0x08E00001U, \
+        SM_CFG_W1(0x0001a350U), 0x08E00002U, \
         SM_CFG_W1(0x0001a354U), 0x0FFFFC11U, \
         SM_CFG_Z1(0x0001a35cU), \
         SM_CFG_Z1(0x0001a364U), \
         SM_CFG_Z1(0x0001a36cU), \
         SM_CFG_Z1(0x0001a374U), \
         SM_CFG_Z1(0x0001a37cU), \
-        SM_CFG_W1(0x0001a440U), 0x08000001U, \
+        SM_CFG_W1(0x0001a440U), 0x08000002U, \
         SM_CFG_W1(0x0001a444U), 0x089FFC11U, \
         SM_CFG_Z1(0x0001a44cU), \
         SM_CFG_Z1(0x0001a454U), \
@@ -1015,10 +1017,12 @@
         SM_CFG_Z1(0x0001ab4cU), \
         SM_CFG_Z1(0x0001ab54U), \
         SM_CFG_Z1(0x0001ab5cU), \
-        SM_CFG_W1(0x0001ac40U), 0x08B00000U, \
-        SM_CFG_W1(0x0001ac44U), 0x08BFFC11U, \
-        SM_CFG_Z1(0x0001ac4cU), \
-        SM_CFG_Z1(0x0001ac54U), \
+        SM_CFG_W1(0x0001ac40U), 0x08000001U, \
+        SM_CFG_W1(0x0001ac44U), 0x08AFFC11U, \
+        SM_CFG_W1(0x0001ac48U), 0x08B00000U, \
+        SM_CFG_W1(0x0001ac4cU), 0x08BFFC11U, \
+        SM_CFG_W1(0x0001ac50U), 0x08C00001U, \
+        SM_CFG_W1(0x0001ac54U), 0x0FFFFC11U, \
         SM_CFG_Z1(0x0001ac5cU), \
         SM_CFG_Z1(0x00010020U), \
         SM_CFG_Z1(0x00012020U), \
@@ -1154,6 +1158,7 @@
         SM_CFG_W1(0x00010024U), 0x6666U, \
         SM_CFG_W1(0x00010028U), 0x7777U, \
         SM_CFG_W1(0x0001002cU), 0x6600U, \
+        SM_CFG_W1(0x00010040U), 0x00000033U, \
         SM_CFG_Z1(0x00010060U), \
         SM_CFG_W1(0x00010070U), 0x00003000U, \
         SM_CFG_Z1(0x00010260U), \

configs/tqma95xxsa-2gb.cfg
@@ -72,6 +72,9 @@ DATA:               perm=rw
 
 # Resources
 
+# Sharing MP access may not be safe if FuSa SW using EDMA2
+EDMA2_MP            ACCESS
+EDMA2_CH0_1         OWNER
 V2X_ACC             OWNER
 
 # Memory
@@ -103,7 +106,9 @@ V2X_FH              OWNER
 
 # Memory
 
+DDR                 READONLY, begin=0x080000000, end=0x08AFFFFFF, nodbg
 DDR                 DATA, begin=0x08B000000, end=0x08BFFFFFF, nodbg
+DDR                 READONLY, begin=0x08C000000, end=0x0FFFFFFFF, nodbg
 
 #==========================================================================#
 # SM M33 EENV                                                              #

configs/tqma95xxsa-2gb/config_trdc.h
@@ -689,6 +689,7 @@
         SM_CFG_W1(0x000008e0U), 0xB00000ACU, \
         SM_CFG_W1(0x00000900U), 0x90000004U, \
         SM_CFG_W1(0x00000920U), 0xB0000053U, \
+        SM_CFG_W1(0x00000920U), 0xB00000A0U, \
         SM_CFG_W1(0x00000940U), 0xB0000053U, \
         SM_CFG_W1(0x00000960U), 0xB0000053U, \
         SM_CFG_W1(0x00000980U), 0xB0000053U, \
@@ -952,8 +953,9 @@
         SM_CFG_W1(0x00018580U), 0x33333333U, \
         SM_CFG_W1(0x00019240U), 0x99999999U, \
         SM_CFG_W1(0x00019380U), 0x99999999U, \
-        SM_CFG_W1(0x0001a024U), 0x7777U, \
-        SM_CFG_W1(0x0001a028U), 0x7700U, \
+        SM_CFG_W1(0x0001a024U), 0x4444U, \
+        SM_CFG_W1(0x0001a028U), 0x7777U, \
+        SM_CFG_W1(0x0001a02cU), 0x7700U, \
         SM_CFG_W1(0x0001a040U), 0x08000000U, \
         SM_CFG_W1(0x0001a044U), 0x0FFFFC11U, \
         SM_CFG_Z1(0x0001a04cU), \
@@ -963,18 +965,18 @@
         SM_CFG_Z1(0x0001a24cU), \
         SM_CFG_Z1(0x0001a254U), \
         SM_CFG_Z1(0x0001a25cU), \
-        SM_CFG_W1(0x0001a340U), 0x08800001U, \
+        SM_CFG_W1(0x0001a340U), 0x08800002U, \
         SM_CFG_W1(0x0001a344U), 0x089FFC11U, \
-        SM_CFG_W1(0x0001a348U), 0x08A00002U, \
+        SM_CFG_W1(0x0001a348U), 0x08A00003U, \
         SM_CFG_W1(0x0001a34cU), 0x08DFFC01U, \
-        SM_CFG_W1(0x0001a350U), 0x08E00001U, \
+        SM_CFG_W1(0x0001a350U), 0x08E00002U, \
         SM_CFG_W1(0x0001a354U), 0x0FFFFC11U, \
         SM_CFG_Z1(0x0001a35cU), \
         SM_CFG_Z1(0x0001a364U), \
         SM_CFG_Z1(0x0001a36cU), \
         SM_CFG_Z1(0x0001a374U), \
         SM_CFG_Z1(0x0001a37cU), \
-        SM_CFG_W1(0x0001a440U), 0x08000001U, \
+        SM_CFG_W1(0x0001a440U), 0x08000002U, \
         SM_CFG_W1(0x0001a444U), 0x089FFC11U, \
         SM_CFG_Z1(0x0001a44cU), \
         SM_CFG_Z1(0x0001a454U), \
@@ -1015,10 +1017,12 @@
         SM_CFG_Z1(0x0001ab4cU), \
         SM_CFG_Z1(0x0001ab54U), \
         SM_CFG_Z1(0x0001ab5cU), \
-        SM_CFG_W1(0x0001ac40U), 0x08B00000U, \
-        SM_CFG_W1(0x0001ac44U), 0x08BFFC11U, \
-        SM_CFG_Z1(0x0001ac4cU), \
-        SM_CFG_Z1(0x0001ac54U), \
+        SM_CFG_W1(0x0001ac40U), 0x08000001U, \
+        SM_CFG_W1(0x0001ac44U), 0x08AFFC11U, \
+        SM_CFG_W1(0x0001ac48U), 0x08B00000U, \
+        SM_CFG_W1(0x0001ac4cU), 0x08BFFC11U, \
+        SM_CFG_W1(0x0001ac50U), 0x08C00001U, \
+        SM_CFG_W1(0x0001ac54U), 0x0FFFFC11U, \
         SM_CFG_Z1(0x0001ac5cU), \
         SM_CFG_Z1(0x00010020U), \
         SM_CFG_Z1(0x00012020U), \
@@ -1154,6 +1158,7 @@
         SM_CFG_W1(0x00010024U), 0x6666U, \
         SM_CFG_W1(0x00010028U), 0x7777U, \
         SM_CFG_W1(0x0001002cU), 0x6600U, \
+        SM_CFG_W1(0x00010040U), 0x00000033U, \
         SM_CFG_Z1(0x00010060U), \
         SM_CFG_W1(0x00010070U), 0x00003000U, \
         SM_CFG_Z1(0x00010260U), \

configs/tqma95xxsa-4gb.cfg
@@ -72,6 +72,9 @@ DATA:               perm=rw
 
 # Resources
 
+# Sharing MP access may not be safe if FuSa SW using EDMA2
+EDMA2_MP            ACCESS
+EDMA2_CH0_1         OWNER
 V2X_ACC             OWNER
 
 # Memory
@@ -103,7 +106,9 @@ V2X_FH              OWNER
 
 # Memory
 
+DDR                 READONLY, begin=0x080000000, end=0x08AFFFFFF, nodbg
 DDR                 DATA, begin=0x08B000000, end=0x08BFFFFFF, nodbg
+DDR                 READONLY, begin=0x08C000000, end=0x17FFFFFFF, nodbg
 
 #==========================================================================#
 # SM M33 EENV                                                              #

configs/tqma95xxsa-4gb/config_trdc.h
@@ -689,6 +689,7 @@
         SM_CFG_W1(0x000008e0U), 0xB00000ACU, \
         SM_CFG_W1(0x00000900U), 0x90000004U, \
         SM_CFG_W1(0x00000920U), 0xB0000053U, \
+        SM_CFG_W1(0x00000920U), 0xB00000A0U, \
         SM_CFG_W1(0x00000940U), 0xB0000053U, \
         SM_CFG_W1(0x00000960U), 0xB0000053U, \
         SM_CFG_W1(0x00000980U), 0xB0000053U, \
@@ -952,8 +953,9 @@
         SM_CFG_W1(0x00018580U), 0x33333333U, \
         SM_CFG_W1(0x00019240U), 0x99999999U, \
         SM_CFG_W1(0x00019380U), 0x99999999U, \
-        SM_CFG_W1(0x0001a024U), 0x7777U, \
-        SM_CFG_W1(0x0001a028U), 0x7700U, \
+        SM_CFG_W1(0x0001a024U), 0x4444U, \
+        SM_CFG_W1(0x0001a028U), 0x7777U, \
+        SM_CFG_W1(0x0001a02cU), 0x7700U, \
         SM_CFG_W1(0x0001a040U), 0x08000000U, \
         SM_CFG_W1(0x0001a044U), 0x17FFFC11U, \
         SM_CFG_Z1(0x0001a04cU), \
@@ -963,18 +965,18 @@
         SM_CFG_Z1(0x0001a24cU), \
         SM_CFG_Z1(0x0001a254U), \
         SM_CFG_Z1(0x0001a25cU), \
-        SM_CFG_W1(0x0001a340U), 0x08800001U, \
+        SM_CFG_W1(0x0001a340U), 0x08800002U, \
         SM_CFG_W1(0x0001a344U), 0x089FFC11U, \
-        SM_CFG_W1(0x0001a348U), 0x08A00002U, \
+        SM_CFG_W1(0x0001a348U), 0x08A00003U, \
         SM_CFG_W1(0x0001a34cU), 0x08DFFC01U, \
-        SM_CFG_W1(0x0001a350U), 0x08E00001U, \
+        SM_CFG_W1(0x0001a350U), 0x08E00002U, \
         SM_CFG_W1(0x0001a354U), 0x17FFFC11U, \
         SM_CFG_Z1(0x0001a35cU), \
         SM_CFG_Z1(0x0001a364U), \
         SM_CFG_Z1(0x0001a36cU), \
         SM_CFG_Z1(0x0001a374U), \
         SM_CFG_Z1(0x0001a37cU), \
-        SM_CFG_W1(0x0001a440U), 0x08000001U, \
+        SM_CFG_W1(0x0001a440U), 0x08000002U, \
         SM_CFG_W1(0x0001a444U), 0x089FFC11U, \
         SM_CFG_Z1(0x0001a44cU), \
         SM_CFG_Z1(0x0001a454U), \
@@ -1015,10 +1017,12 @@
         SM_CFG_Z1(0x0001ab4cU), \
         SM_CFG_Z1(0x0001ab54U), \
         SM_CFG_Z1(0x0001ab5cU), \
-        SM_CFG_W1(0x0001ac40U), 0x08B00000U, \
-        SM_CFG_W1(0x0001ac44U), 0x08BFFC11U, \
-        SM_CFG_Z1(0x0001ac4cU), \
-        SM_CFG_Z1(0x0001ac54U), \
+        SM_CFG_W1(0x0001ac40U), 0x08000001U, \
+        SM_CFG_W1(0x0001ac44U), 0x08AFFC11U, \
+        SM_CFG_W1(0x0001ac48U), 0x08B00000U, \
+        SM_CFG_W1(0x0001ac4cU), 0x08BFFC11U, \
+        SM_CFG_W1(0x0001ac50U), 0x08C00001U, \
+        SM_CFG_W1(0x0001ac54U), 0x17FFFC11U, \
         SM_CFG_Z1(0x0001ac5cU), \
         SM_CFG_Z1(0x00010020U), \
         SM_CFG_Z1(0x00012020U), \
@@ -1154,6 +1158,7 @@
         SM_CFG_W1(0x00010024U), 0x6666U, \
         SM_CFG_W1(0x00010028U), 0x7777U, \
         SM_CFG_W1(0x0001002cU), 0x6600U, \
+        SM_CFG_W1(0x00010040U), 0x00000033U, \
         SM_CFG_Z1(0x00010060U), \
         SM_CFG_W1(0x00010070U), 0x00003000U, \
         SM_CFG_Z1(0x00010260U), \

commit 248a477b71a5b9b7085a6812c8d5746c6d473a83

tqma94xxla/tqma95xxla/tqma95xxsa: fix SM-339: Allow V2X fast hash after ROM phase
Changing OWNER for EDMA2_CH0_1 needs removing the old owner. Since the DMA channel was moved
to ELE Domain, it must be removed from A55. Also allow sharing of EDMA2_MP. Both changes were
missed when porting to lf-6.18.2-1.0.0.

fixes: 07c1bf78ec5e ("tqma94xxla/tqma95xxla/tqma95xxsa: SM-339: Allow V2X fast hash after ROM phase")

Signed-off-by: Markus Niebel <Markus.Niebel@ew.tq-group.com>

configs/tqma95xxla-4gb.cfg
@@ -683,8 +683,7 @@ EDMA1_CH28          OWNER
 EDMA1_CH29          OWNER
 EDMA1_CH30          OWNER
 EDMA1_CH31          OWNER
-EDMA2_MP            OWNER
-EDMA2_CH0_1         OWNER
+EDMA2_MP            ACCESS
 EDMA2_CH2_3         OWNER
 EDMA2_CH4_5         OWNER
 EDMA2_CH6_7         OWNER

configs/tqma95xxla-4gb/config_trdc.h
@@ -688,7 +688,6 @@
         SM_CFG_W1(0x000008c0U), 0xB00000A9U, \
         SM_CFG_W1(0x000008e0U), 0xB00000ACU, \
         SM_CFG_W1(0x00000900U), 0x90000004U, \
-        SM_CFG_W1(0x00000920U), 0xB0000053U, \
         SM_CFG_W1(0x00000920U), 0xB00000A0U, \
         SM_CFG_W1(0x00000940U), 0xB0000053U, \
         SM_CFG_W1(0x00000960U), 0xB0000053U, \
@@ -1169,7 +1168,7 @@
         SM_CFG_W1(0x000105d0U), 0x33333333U, \
         SM_CFG_W1(0x000105d4U), 0x33333333U, \
         SM_CFG_W1(0x000105d8U), 0x00000003U, \
-        SM_CFG_W1(0x00010640U), 0x99999999U, \
+        SM_CFG_W1(0x00010640U), 0x99999909U, \
         SM_CFG_W1(0x00010644U), 0x99999999U, \
         SM_CFG_W1(0x00010648U), 0x99999999U, \
         SM_CFG_W1(0x0001064cU), 0x99999999U, \

configs/tqma95xxsa-2gb-m7.cfg
@@ -665,8 +665,7 @@ EDMA1_CH28          OWNER
 EDMA1_CH29          OWNER
 EDMA1_CH30          OWNER
 EDMA1_CH31          OWNER
-EDMA2_MP            OWNER
-EDMA2_CH0_1         OWNER
+EDMA2_MP            ACCESS
 EDMA2_CH2_3         OWNER
 EDMA2_CH4_5         OWNER
 EDMA2_CH6_7         OWNER

configs/tqma95xxsa-2gb-m7/config_trdc.h
@@ -688,7 +688,6 @@
         SM_CFG_W1(0x000008c0U), 0xB00000A9U, \
         SM_CFG_W1(0x000008e0U), 0xB00000ACU, \
         SM_CFG_W1(0x00000900U), 0x90000004U, \
-        SM_CFG_W1(0x00000920U), 0xB0000053U, \
         SM_CFG_W1(0x00000920U), 0xB00000A0U, \
         SM_CFG_W1(0x00000940U), 0xB0000053U, \
         SM_CFG_W1(0x00000960U), 0xB0000053U, \
@@ -1169,7 +1168,7 @@
         SM_CFG_W1(0x000105d0U), 0x33333333U, \
         SM_CFG_W1(0x000105d4U), 0x33333333U, \
         SM_CFG_W1(0x000105d8U), 0x00000003U, \
-        SM_CFG_W1(0x00010640U), 0x99999999U, \
+        SM_CFG_W1(0x00010640U), 0x99999909U, \
         SM_CFG_W1(0x00010644U), 0x99999999U, \
         SM_CFG_W1(0x00010648U), 0x99999999U, \
         SM_CFG_W1(0x0001064cU), 0x99999999U, \

configs/tqma95xxsa-2gb.cfg
@@ -660,8 +660,7 @@ EDMA1_CH28          OWNER
 EDMA1_CH29          OWNER
 EDMA1_CH30          OWNER
 EDMA1_CH31          OWNER
-EDMA2_MP            OWNER
-EDMA2_CH0_1         OWNER
+EDMA2_MP            ACCESS
 EDMA2_CH2_3         OWNER
 EDMA2_CH4_5         OWNER
 EDMA2_CH6_7         OWNER

configs/tqma95xxsa-2gb/config_trdc.h
@@ -688,7 +688,6 @@
         SM_CFG_W1(0x000008c0U), 0xB00000A9U, \
         SM_CFG_W1(0x000008e0U), 0xB00000ACU, \
         SM_CFG_W1(0x00000900U), 0x90000004U, \
-        SM_CFG_W1(0x00000920U), 0xB0000053U, \
         SM_CFG_W1(0x00000920U), 0xB00000A0U, \
         SM_CFG_W1(0x00000940U), 0xB0000053U, \
         SM_CFG_W1(0x00000960U), 0xB0000053U, \
@@ -1169,7 +1168,7 @@
         SM_CFG_W1(0x000105d0U), 0x33333333U, \
         SM_CFG_W1(0x000105d4U), 0x33333333U, \
         SM_CFG_W1(0x000105d8U), 0x00000003U, \
-        SM_CFG_W1(0x00010640U), 0x99999999U, \
+        SM_CFG_W1(0x00010640U), 0x99999909U, \
         SM_CFG_W1(0x00010644U), 0x99999999U, \
         SM_CFG_W1(0x00010648U), 0x99999999U, \
         SM_CFG_W1(0x0001064cU), 0x99999999U, \

configs/tqma95xxsa-4gb.cfg
@@ -660,8 +660,7 @@ EDMA1_CH28          OWNER
 EDMA1_CH29          OWNER
 EDMA1_CH30          OWNER
 EDMA1_CH31          OWNER
-EDMA2_MP            OWNER
-EDMA2_CH0_1         OWNER
+EDMA2_MP            ACCESS
 EDMA2_CH2_3         OWNER
 EDMA2_CH4_5         OWNER
 EDMA2_CH6_7         OWNER

configs/tqma95xxsa-4gb/config_trdc.h
@@ -688,7 +688,6 @@
         SM_CFG_W1(0x000008c0U), 0xB00000A9U, \
         SM_CFG_W1(0x000008e0U), 0xB00000ACU, \
         SM_CFG_W1(0x00000900U), 0x90000004U, \
-        SM_CFG_W1(0x00000920U), 0xB0000053U, \
         SM_CFG_W1(0x00000920U), 0xB00000A0U, \
         SM_CFG_W1(0x00000940U), 0xB0000053U, \
         SM_CFG_W1(0x00000960U), 0xB0000053U, \
@@ -1169,7 +1168,7 @@
         SM_CFG_W1(0x000105d0U), 0x33333333U, \
         SM_CFG_W1(0x000105d4U), 0x33333333U, \
         SM_CFG_W1(0x000105d8U), 0x00000003U, \
-        SM_CFG_W1(0x00010640U), 0x99999999U, \
+        SM_CFG_W1(0x00010640U), 0x99999909U, \
         SM_CFG_W1(0x00010644U), 0x99999999U, \
         SM_CFG_W1(0x00010648U), 0x99999999U, \
         SM_CFG_W1(0x0001064cU), 0x99999999U, \